9081fdc0d7218ac7a7b9e79ad5dab1452dcf9827
MOHAMMAD ZOHEB SAJAN <mzohebsajan@example.com>
2025-03-13 22:50:21 +0530

    Initial Setup

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, zoheb;\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
